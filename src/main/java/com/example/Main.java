2ec6e19b7112e6bab800d5b1b3b9f53ddd4c385f
Muthia  Athaya <athayamuthia@gmail.com>
2024-01-31 15:00:31 +0700

    First Commit Java Hello World

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,18 @@
+package com.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//test
+        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+        System.out.print("Hello and welcome!");
+
+        boolean flag;
+        float num;
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
